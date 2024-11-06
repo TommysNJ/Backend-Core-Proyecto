8f5b34fb4cad33ba5ed916ea45b09a73ad52000c
TommysNJ <TommysNJ@users.noreply.github.com>
2024-11-06 14:07:20 -0500

    Update alumnoController 1

--- node/routes/alumnoRoutes.js
+++ node/routes/alumnoRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/', verifyToken, verifyRole([3]), createAlumno); // no va a ser necesario
 router.get('/', verifyToken, verifyRole([3]), getAllAlumnos);
 router.get('/:email', verifyToken, verifyRole([3, 1]), getAlumnoByEmail); // Ver un alumno por su email (administrador y alumno)
-router.put('/:email', verifyToken, verifyRole([3]), updateAlumno);
+router.put('/:email', verifyToken, verifyRole([3, 1]), updateAlumno);
 router.delete('/:email', verifyToken, verifyRole([3]), deleteAlumno); // no va a ser necesario
 
 export default router;

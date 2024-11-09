d353f9f1dee0c054e9e182c7cb66e552cb1c7b26
TommysNJ <TommysNJ@users.noreply.github.com>
2024-11-09 03:45:30 -0500

    permisos instructor progresoRoutes

--- node/routes/progresoRoutes.js
+++ node/routes/progresoRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Rutas para actualizar y ver progreso (solo alumnos)
 router.post('/', verifyToken, verifyRole([1]), updateProgreso); // Solo alumnos actualizan progreso
-router.get('/:id_inscripcion', verifyToken, verifyRole([1]), getProgreso); // Solo alumnos ven el progreso
+router.get('/:id_inscripcion', verifyToken, verifyRole([1, 2]), getProgreso); // Solo alumnos e instructores ven el progreso
 router.delete('/:id_detalle', verifyToken, verifyRole([1]), deleteProgreso); // Solo alumnos pueden eliminar su propio progreso
 
 export default router;

30576c397183cf50f6cbea22f8b0b0973344e4ab
TommysNJ <TommysNJ@users.noreply.github.com>
2024-11-28 21:11:48 -0500

    Update reportRoutes

--- node/routes/reportRoutes.js
+++ node/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken, verifyRole } from "../middleware/auth.js";
-import { getPopularidadTemas, getPorcentajeInscripcionesPorTematica, getPopularidadTemasConFiltros } from "../controllers/ReportController.js";
+import { getPopularidadTemas, getPorcentajeInscripcionesPorTematica, getPopularidadTemasConFiltros, getPopularidadSubtematicasPorInstructor } from "../controllers/ReportController.js";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/popularidad-temas", verifyToken, verifyRole([3]), getPopularidadTemas);
 router.get('/porcentaje-inscripciones', verifyToken, verifyRole([3]), getPorcentajeInscripcionesPorTematica);
 router.get("/popularidad-temas-filtros", verifyToken, verifyRole([3]), getPopularidadTemasConFiltros);
-
+// Ruta para el reporte de popularidad de subtemáticas por instructor
+router.get("/popularidad-subtematicas-instructor", verifyToken, verifyRole([3]), getPopularidadSubtematicasPorInstructor);
 
 export default router;
